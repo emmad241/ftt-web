59b3ea3b9ecb27f913ed86acbfea1224444bf621
emmad241 <emmaduffy241@gmail.com>
2022-10-18 16:32:49 +0100

    Got login functioning

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const User = require('../config');
 
 /* GET login page. */
-router.get('/', async (req, res) => {
-  res.render('../views/pages/login', { pageTitle: "Login" });
+router.get("/", async (req, res) => {
+	res.render("../views/pages/login", { pageTitle: "Login" });
+});
+
+router.post("/", async (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+
+    const snapshot = await User.get();
+    const list = snapshot.docs.map((doc)=>doc.data());
+
+    for(const row of list) {
+        if(username == row.username && password == row.password){
+            res.redirect("/");
+        }
+    };
+
 });
 
 module.exports = router;
